abdb4a5f331d0a34cd4d2374d3a014878ae5b6cd
Karel Jilek <los.karlosss@gmail.com>
2021-04-11 17:38:09 +0200

    add verify for load and store

--- benchmarks/immintrin_benchmarks/test_include.h
+++ benchmarks/immintrin_benchmarks/test_include.h
@@ -3,6 +3,22 @@
 #include "igen_dd_immintrin.h"
 #include "igen_dd_lib.h"
 
+void mm256_loadu_pd_base(double* in, double* out);
+void mm256_loadu_pd_verify(dd_I* in, dd_I* out);
+void mm256_loadu_pd_benchmark(dd_I* in, dd_I* out);
+
+void mm256_load_pd_base(double* in, double* out);
+void mm256_load_pd_verify(dd_I* in, dd_I* out);
+void mm256_load_pd_benchmark(dd_I* in, dd_I* out);
+
+void mm256_storeu_pd_base(double* in, double* out);
+void mm256_storeu_pd_verify(dd_I* in, dd_I* out);
+void mm256_storeu_pd_benchmark(dd_I* in, dd_I* out);
+
+void mm256_store_pd_base(double* in, double* out);
+void mm256_store_pd_verify(dd_I* in, dd_I* out);
+void mm256_store_pd_benchmark(dd_I* in, dd_I* out);
+
 void mm256_blend_pd_base(double* in, double* out);
 void mm256_blend_pd_verify(dd_I* in, dd_I* out);
 void mm256_blend_pd_benchmark(dd_I* in, dd_I* out);
