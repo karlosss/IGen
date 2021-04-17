53fb7f13b22e0a8895a97de54f25d82e168e1de8
Karel Jilek <los.karlosss@gmail.com>
2021-04-17 18:40:22 +0200

    alter add and blend benchmark to new code format

--- benchmarks/immintrin_benchmarks/register.h
+++ benchmarks/immintrin_benchmarks/register.h
@@ -2,9 +2,19 @@
 
 #include "benchmark_macros.h"
 
+#include "benchmarks/mm256_add_pd.h"
+#include "benchmarks/mm256_mul_pd.h"
+#include "benchmarks/mm256_blend_pd.h"
+
 BENCHMARK(mm256_mul_pd, fb)
 BENCHMARK(mm256_mul_pd, forloop)
 BENCHMARK(mm256_mul_pd, inlined_loop)
+
+BENCHMARK(mm256_blend_pd, fb)
+BENCHMARK(mm256_blend_pd, op)
+
+BENCHMARK(mm256_add_pd, fb)
+BENCHMARK(mm256_add_pd, op)
 
 static void register_functions() {
     // first one is the baseline function that is used to verify against
@@ -12,4 +22,10 @@ static void register_functions() {
     ADD_FUNC(mm256_mul_pd, fb, 16, 12)
     ADD_FUNC(mm256_mul_pd, forloop, 16, 12)
     ADD_FUNC(mm256_mul_pd, inlined_loop, 16, 12)
+
+    ADD_FUNC(mm256_blend_pd, fb, 16, 12)
+    ADD_FUNC(mm256_blend_pd, op, 16, 12)
+
+    ADD_FUNC(mm256_add_pd, fb, 16, 12)
+    ADD_FUNC(mm256_add_pd, op, 16, 12)
 }
