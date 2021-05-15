624ea4cf2d6ce58909ba0f484344c0d724f16cd5
Karel Jilek <los.karlosss@gmail.com>
2021-05-15 22:42:57 +0200

    add writeup

--- clang_test/tests/basic.cpp
+++ clang_test/tests/basic.cpp
@@ -2,8 +2,8 @@ int main() {
     double a, b, c, d;
     a = 1 + 2.5;
     b = 42 + 69;
-    c = a + b;
-    a = c + 5;
-    a = a + b;
+    c = a + b; // 1 + 2.5 + b
+    a = c + 5; // 1 + 2.5 + b + 5
+    a = a + b; // 1 + 2.5 + b + 5 + b
     return 0;
 }
