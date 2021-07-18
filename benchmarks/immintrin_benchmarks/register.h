c8872542f24b0bdffdf9269e1549b0f9cabea81b
Karel Jilek <los.karlosss@gmail.com>
2021-07-18 10:24:20 +0200

    add fma

--- benchmarks/immintrin_benchmarks/register.h
+++ benchmarks/immintrin_benchmarks/register.h
@@ -6,6 +6,7 @@
 #include "benchmarks/mm256_mul_pd.h"
 #include "benchmarks/mm256_sub_pd.h"
 #include "benchmarks/mm256_div_pd.h"
+#include "benchmarks/mm256_fmadd_pd.h"
 
 BENCHMARK(mm256_sub_pd, fb)
 BENCHMARK(mm256_sub_pd, permute_no_unused_vals)
@@ -24,10 +25,15 @@ BENCHMARK(mm256_add_pd, inlined_loop)
 BENCHMARK(mm256_add_pd, one_inlined_fn)
 BENCHMARK(mm256_add_pd, interleaved)
 BENCHMARK(mm256_add_pd, no_unused_vals)
+BENCHMARK(mm256_add_pd, transposed)
 
 BENCHMARK(mm256_div_pd, fb)
 BENCHMARK(mm256_div_pd, inline_fn)
 BENCHMARK(mm256_div_pd, transposed)
+
+BENCHMARK(mm256_fmadd_pd, fb)
+BENCHMARK(mm256_fmadd_pd, sep)
+BENCHMARK(mm256_fmadd_pd, op)
 
 static void register_functions() {
     // first one is the baseline function that is used to verify against
@@ -35,22 +41,27 @@ static void register_functions() {
 //    ADD_FUNC(mm256_sub_pd, fb, 2, 2)
 //    ADD_FUNC(mm256_sub_pd, permute_no_unused_vals, 2, 2)
 //
-    ADD_FUNC(mm256_mul_pd, fb, 16, 12)
+//    ADD_FUNC(mm256_mul_pd, fb, 16, 12)
 //    ADD_FUNC(mm256_mul_pd, inlined_loop, 16, 12)
 //    ADD_FUNC(mm256_mul_pd, one_inlined_fn, 16, 12)
 //    ADD_FUNC(mm256_mul_pd, no_interleave, 16, 12)
 //    ADD_FUNC(mm256_mul_pd, mul_fn, 16, 12)
 //    ADD_FUNC(mm256_mul_pd, interleaved, 16, 12)
-    ADD_FUNC(mm256_mul_pd, transposed, 16, 12)
-//
+//    ADD_FUNC(mm256_mul_pd, transposed, 16, 12)
+
 //    ADD_FUNC(mm256_add_pd, fb, 16, 12)
 //    ADD_FUNC(mm256_add_pd, forloop, 16, 12)
 //    ADD_FUNC(mm256_add_pd, inlined_loop, 16, 12)
 //    ADD_FUNC(mm256_add_pd, one_inlined_fn, 16, 12)
 //    ADD_FUNC(mm256_add_pd, interleaved, 16, 12)
 //    ADD_FUNC(mm256_add_pd, no_unused_vals, 16, 12)
+//    ADD_FUNC(mm256_add_pd, transposed, 16, 12)
 
     ADD_FUNC(mm256_div_pd, fb, 16, 12)
     ADD_FUNC(mm256_div_pd, inline_fn, 16, 12)
     ADD_FUNC(mm256_div_pd, transposed, 16, 12)
+
+    ADD_FUNC(mm256_fmadd_pd, fb, 16, 12)
+    ADD_FUNC(mm256_fmadd_pd, sep, 16, 12)
+    ADD_FUNC(mm256_fmadd_pd, op, 16, 12)
 }
