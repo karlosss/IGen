2d05506a01d36141c48a15ab2af2b1f9ed2cb025
Karel Jilek <los.karlosss@gmail.com>
2021-07-20 17:49:39 +0200

    add anomaly

--- benchmarks/immintrin_benchmarks/register.h
+++ benchmarks/immintrin_benchmarks/register.h
@@ -58,12 +58,39 @@ static void register_functions() {
 //    ADD_FUNC(mm256_add_pd, no_unused_vals, 16, 12)
 //    ADD_FUNC(mm256_add_pd, transposed, 16, 12)
 //
-//    ADD_FUNC(mm256_div_pd, fb, 16, 12)
-//    ADD_FUNC(mm256_div_pd, inline_fn, 16, 12)
-//    ADD_FUNC(mm256_div_pd, transposed, 16, 12)
-//    ADD_FUNC(mm256_div_pd, transposed_avx_cond, 16, 12)
+    ADD_FUNC(mm256_div_pd, fb, 16, 12)
+    ADD_FUNC(mm256_div_pd, inline_fn, 16, 12)
+    ADD_FUNC(mm256_div_pd, transposed, 16, 12)
+    ADD_FUNC(mm256_div_pd, transposed_avx_cond, 16, 12)
 
     ADD_FUNC(mm256_fmadd_pd, fb, 16, 12)
     ADD_FUNC(mm256_fmadd_pd, sep, 16, 12)
     ADD_FUNC(mm256_fmadd_pd, op, 16, 12)
 }
+
+
+//2 functions registered.
+//Verify mm256_fmadd_pd
+//sep... OK
+//op... OK
+//Verify mm256_div_pd
+//inline_fn... OK
+//transposed... OK
+//transposed_avx_cond... OK
+//Running benchmarks.
+//Benchmark mm256_fmadd_pd
+//fb latency: 394.413 cycles
+//        fb gap: 389.084 cycles
+//        sep latency: 155.695 cycles
+//        sep gap: 141.717 cycles
+//        op latency: 219.901 cycles
+//        op gap: 213.278 cycles
+//        Benchmark mm256_div_pd
+//        fb latency: 767.383 cycles
+//        fb gap: 123.311 cycles
+//        inline_fn latency: 630.579 cycles
+//        inline_fn gap: 32.317 cycles
+//        transposed latency: 259.411 cycles
+//        transposed gap: 267.577 cycles
+//        transposed_avx_cond latency: 248.99 cycles
+//        transposed_avx_cond gap: 242.179 cycles
